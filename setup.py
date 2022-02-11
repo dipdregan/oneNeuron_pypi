22a12020728b13f3fe44b3563670e88aec8bcc17
Dipendra Pratap <dipendrapratap155@gmail.com>
2022-02-11 03:19:43 -0800

    updateing setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ USER_NAME='dipdregan'
 
 setuptools.setup(
     name=f"{PROJECT_NAME}-{USER_NAME}",
-    version="0.0.1",
+    version="0.0.3",
     author="Dipendra Pratap",
     
     description="It's implementation of perceptron",
